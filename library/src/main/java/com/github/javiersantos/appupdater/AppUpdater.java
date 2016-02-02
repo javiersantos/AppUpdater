687ca8f39b941609f007818c633b39b8c12a686c
Javier Santos <me@javiersantos.me>
2016-02-02 21:54:11 +0100

    More documentation

--- library/src/main/java/com/github/javiersantos/appupdater/AppUpdater.java
+++ library/src/main/java/com/github/javiersantos/appupdater/AppUpdater.java
@@ -25,26 +25,60 @@ public class AppUpdater {
         this.showAppUpdated = false;
     }
 
+    /**
+     * Set the type of message used to notify the user when a new update has been found. Default: DIALOG.
+     *
+     * @param display how the update will be shown
+     * @see com.github.javiersantos.appupdater.enums.Display
+     */
     public void setDisplay(Display display) {
         this.display = display;
     }
 
+    /**
+     * Set the source where the latest update can be found. Default: GOOGLE_PLAY.
+     *
+     * @param updateFrom source where the latest update is uploaded. If GITHUB is selected, .setGitHubAndRepo method is required.
+     * @see com.github.javiersantos.appupdater.enums.UpdateFrom
+     */
     public void setUpdateFrom(UpdateFrom updateFrom) {
         this.updateFrom = updateFrom;
     }
 
+    /**
+     * Set the duration of the Snackbar Default: NORMAL.
+     *
+     * @param duration duration of the Snackbar
+     * @see com.github.javiersantos.appupdater.enums.Duration
+     */
     public void setDuration(Duration duration) {
         this.duration = duration;
     }
 
+    /**
+     * Set the user and repo where the releases are uploaded. You must upload your updates as a release in order to work properly taggin them as vX.X.X or X.X.X.
+     *
+     * @param user GitHub user
+     * @param repo GitHub repository
+     */
     public void setGitHubUserAndRepo(String user, String repo) {
         this.gitHub = new GitHub(user, repo);
     }
 
+    /**
+     * Set the times the app ascertains that a new update is available and display a dialog, Snackbar or notification. It makes the updates less invasive. Default: 5.
+     *
+     * @param times every X times
+     */
     public void showEvery(Integer times) {
         this.showEvery = times;
     }
 
+    /**
+     * Set if the dialog, Snackbar or notification is displayed although there aren't updates. Default: false.
+     *
+     * @param res
+     */
     public void showAppUpdated(Boolean res) {
         this.showAppUpdated = res;
     }
