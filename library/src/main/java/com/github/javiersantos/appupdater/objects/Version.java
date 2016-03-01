1aeee95e20025b1eacadd61e193f415ea191bb2f
Deletescape <deletescape@outlook.com>
2016-03-01 21:55:57 +0100

    formatting

--- library/src/main/java/com/github/javiersantos/appupdater/objects/Version.java
+++ library/src/main/java/com/github/javiersantos/appupdater/objects/Version.java
@@ -11,33 +11,35 @@ public class Version implements Comparable<Version> {
     }
 
     public Version(String version) {
-        if(version == null)
+        if (version == null)
             Log.e(TAG, "Version can not be null");
-        else if(!version.matches("[0-9]+(\\.[0-9]+)*"))
+        else if (!version.matches("[0-9]+(\\.[0-9]+)*"))
             Log.e(TAG, "Invalid version format");
         this.version = version;
     }
 
-    @Override public int compareTo(Version that) {
-        if(that == null)
+    @Override
+    public int compareTo(Version that) {
+        if (that == null)
             return 1;
         String[] thisParts = this.get().split("\\.");
         String[] thatParts = that.get().split("\\.");
         int length = Math.max(thisParts.length, thatParts.length);
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             int thisPart = i < thisParts.length ?
                     Integer.parseInt(thisParts[i]) : 0;
             int thatPart = i < thatParts.length ?
                     Integer.parseInt(thatParts[i]) : 0;
-            if(thisPart < thatPart)
+            if (thisPart < thatPart)
                 return -1;
-            if(thisPart > thatPart)
+            if (thisPart > thatPart)
                 return 1;
         }
         return 0;
     }
 
-    @Override public boolean equals(Object that) {
+    @Override
+    public boolean equals(Object that) {
         if (this == that)
             return true;
         if (that == null)
