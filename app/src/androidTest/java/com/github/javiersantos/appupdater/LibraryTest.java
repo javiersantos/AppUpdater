c8d116929db8a5ae0470416dc667ad2401d66cbd
Javier Santos <me@javiersantos.me>
2018-07-05 22:55:28 +0200

    Update Test cases

--- app/src/androidTest/java/com/github/javiersantos/appupdater/LibraryTest.java
+++ app/src/androidTest/java/com/github/javiersantos/appupdater/LibraryTest.java
@@ -46,6 +46,41 @@ public class LibraryTest {
     }
 
     @Test
+    public void Version_UpdateAvailable_4() {
+        Update installedUpdate = new Update();
+        installedUpdate.setLatestVersion("2.1.3.alpha.42.RC");
+
+        Update latestUpdate = new Update();
+        latestUpdate.setLatestVersion("2.1.3.alpha.45.RC");
+
+        assertTrue(UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateAvailable_1() {
+        Update installedUpdate = new Update("1.0.0", 1);
+        Update latestUpdate = new Update("1.0.0", 2);
+
+        assertTrue(UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateAvailable_2() {
+        Update installedUpdate = new Update("1.0.0", 1);
+        Update latestUpdate = new Update("1.0.1", 2);
+
+        assertTrue(UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateAvailable_3() {
+        Update installedUpdate = new Update("1.0.0", 1);
+        Update latestUpdate = new Update("0.9.0", 2);
+
+        assertTrue(UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
     public void Version_UpdateNotAvailable_1() {
         Update installedUpdate = new Update();
         installedUpdate.setLatestVersion("1.0.9");
@@ -63,6 +98,30 @@ public class LibraryTest {
 
         Update latestUpdate = new Update();
         latestUpdate.setLatestVersion("1.0.9.0");
+
+        assertTrue(!UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateNotAvailable_1() {
+        Update installedUpdate = new Update("1.0.0", 1);
+        Update latestUpdate = new Update("1.0.0", 1);
+
+        assertTrue(!UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateNotAvailable_2() {
+        Update installedUpdate = new Update("1.0.0", 2);
+        Update latestUpdate = new Update("1.0.1", 1);
+
+        assertTrue(!UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
+    }
+
+    @Test
+    public void VersionCode_UpdateNotAvailable_3() {
+        Update installedUpdate = new Update("1.0.0", 1);
+        Update latestUpdate = new Update("1.2.0", 1);
 
         assertTrue(!UtilsLibrary.isUpdateAvailable(installedUpdate, latestUpdate));
     }
